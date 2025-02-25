15dcb870aaf6f6703af300c0ed1164255b8ff6a3
Chuvaana <chukana.1106gmail.com>
2025-02-25 23:13:04 +0800

    edit

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ require("./app/routes/branch.routes")(app);
 
 
 // set port, listen for requests
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
