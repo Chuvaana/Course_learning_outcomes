92a29a4ec3d88f9c0793a0ebdb77a193705067a8
Chuvaana <chukana.1106gmail.com>
2025-04-06 17:21:10 +0800

    l

---
+++ client/src/app/components/layout/teacher/report-lesson-list/report-lesson-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-report-lesson-list',
+  imports: [],
+  templateUrl: './report-lesson-list.component.html',
+  styleUrl: './report-lesson-list.component.scss'
+})
+export class ReportLessonListComponent {
+
+}
